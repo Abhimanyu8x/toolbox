7c71d4e94efc68637c7355dbe4c324c82e1bcf2c
Abhimanyu Dubey <abhimanyudubey142@gmail.com>
2026-06-27 12:39:19 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Routes,Route} from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { allTools } from './data/tools';
 import toolKeywords from './data/toolKeywords';
